ef1515b0daf2a1d7c198c66e0adeb46b0052ec6a
Guido-95 <guidocarna1995@gmail.com>
2021-06-09 16:03:19 +0200

    inizio progetto

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <Header/>
+    </header>
+
+    <main>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-2 col-md-3 col-sm-4">
+
+          </div>
+          <div class="col-lg-2 col-md-3 col-sm-4">
+
+          </div>
+          <div class="col-lg-2 col-md-3 col-sm-4">
+
+          </div>
+          <div class="col-lg-2 col-md-3 col-sm-4">
+
+          </div>
+          <div class="col-lg-2 col-md-3 col-sm-4">
+
+          </div>
+        </div>
+      </div>
+    </main>
+    
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header:Header,
+
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import '~bootstrap';
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  header{
+    display: flex;
+    align-items:center;
+    height: 80px;
+    background-color:#2e3a46 ;
+  }
+
+  main{
+    height: calc(100vh - 80px);
+    background-color:#1e2d3b;
+  }
+
+  .container{
+    height: 100%;
+    background-color: chartreuse;
+    .row{
+      padding: 100px;
+      justify-content: center;
+      
+      div {
+      height: 300px;
+      border: 1px solid black;
+      background-color: yellow;
+      }
+
+    }
+    
+  }
+
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <img src="../assets/img/spotify.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style lang="scss" scoped>
+
+   div {
+       
+
+       img{
+           margin-left: 40px;
+           width: 70px;
+       }
+   }
+</style>
